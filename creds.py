11c5a717446728437d5987596a1b12ab66ae3f6f
eldhomv <64636539+eldhomv@users.noreply.github.com>
2020-07-10 16:37:29 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "1248362955:AAFU0WRWuJY-E4HfY6eG4uCOpby7zKzY-es"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1FCkAdWA7zqrlShpxQCiQ4hwP5rtZ0_Ca"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0ADj0p4NytnqUUk9PVA"
     
     
     
